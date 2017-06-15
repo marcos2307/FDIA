6c5a4905b5f1b37db74daf2099b85824c00118dc
marcos2307 <gomezredondomarcos@gmail.com>
2017-06-15 17:33:35 -0400

    Solucion para aerofotogrametria Proyecto de pueba para opencv compilado con VS2017

---
+++ FotogrametriaAerea/HolaOpenCV/HolaOpenCV.cpp
@@ -0,0 +1,13 @@
+// HolaOpenCV.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hola" << endl;
+	system("pause");
+}
+
