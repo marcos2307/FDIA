545d60e3510aa43329e9a82681a62cf59ee4e0e2
marcos2307 <gomezredondomarcos@gmail.com>
2017-07-27 12:55:53 -0400

    se terminaron la clase Camera y la funcion generatePLYCameras

--- FotogrametriaAerea/AutoReconstruction/includes.h
+++ FotogrametriaAerea/AutoReconstruction/includes.h
@@ -26,3 +26,59 @@ struct ImageInfo //usada en getImageInfo
 	double pitch;
 	double roll;
 };
+
+class Camera 
+{
+private:
+	vector <Point3d> points = vector <Point3d>(5);
+	double w;
+	double h;
+	Mat rCam = Mat::Mat(Size(3, 3), CV_64FC1);
+	Mat tCam = Mat::Mat(Size(1, 3), CV_64FC1);
+public:
+	Camera(Mat rCam = Mat::eye(3,3,CV_64FC1), Mat tCam = Mat(Size(1, 3), CV_64FC1, { 0, 0, 0 }), double w = 0.04, double h = 0.03)
+	{
+		this->w = w;
+		this->h = h;
+		cout << "entro" << endl;
+		this->points[0] = (Point3d(0, 0, 0));
+		this->points[1] = (Point3d(w / 2, h / 2, 0.02));
+		this->points[2] = (Point3d(w / 2, -h / 2, 0.02));
+		this->points[3] = (Point3d(-w / 2, -h / 2, 0.02));
+		this->points[4] = (Point3d(-w / 2, h / 2, 0.02));
+		this->tCam = tCam.clone();
+		this->rCam = rCam.clone();
+	}
+
+	vector<Point3d> getPoints(void) 
+	{
+		return points;
+	}
+	Mat getR(void)
+	{
+		return rCam;
+	}
+	Mat getT(void)
+	{
+		return tCam;
+	}
+	
+	void rotoTrans(Mat R, Mat t)
+	{
+		rCam = R * rCam;
+		tCam = tCam + t;
+		Mat Temp = Mat::Mat(Size(1, 3), CV_64FC1);
+		Temp = Mat(points, CV_64FC1).reshape(1);
+		cout <<"Temp: "<< Temp << endl;
+		cout << "Temp.Type: " << Temp.type() << " R: " << R.type() << endl;
+		Temp =  (R*Temp.t()).t();
+		for (int i = 0; i < 5; ++i)
+		{
+			Temp.row(i) = Temp.row(i) + t.t();
+		}
+		cout << "R*Temp: " << Temp << endl;
+		cout << "posttemp" << endl;
+		Temp.reshape(3).copyTo(points);
+	}
+
+};
