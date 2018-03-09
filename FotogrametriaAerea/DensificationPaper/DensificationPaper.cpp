ffb2c14de57580374b3db36d3480a10132624b07
marcos2307 <gomezredondomarcos@gmail.com>
2018-03-08 23:05:29 -0300

    08-03-2018.

--- FotogrametriaAerea/DensificationPaper/DensificationPaper.cpp
+++ FotogrametriaAerea/DensificationPaper/DensificationPaper.cpp
@@ -116,6 +116,7 @@ void reconstruct(vector <Mat> images, vector <Mat> grayImages, String camFile, v
 	FileStorage f(camFile, cv::FileStorage::READ, cv::String());
 	Mat K, dist;
 	f["K"] >> K;
+	K.convertTo(K, CV_32FC1);
 	f["dist"] >> dist;
 	cout << "K dist:" << endl << K << dist << endl;
 
@@ -144,10 +145,8 @@ void reconstruct(vector <Mat> images, vector <Mat> grayImages, String camFile, v
 	Rect rect1 = Rect(0, 0, imag1.cols, imag1.rows);
 	Rect rect2 = Rect(imag1.cols, 0, imag1.cols, imag1.rows);
 
-
 	vector< miMatch > seed, local;
 	vector < miMatch > map;
-
 	for (int i = 0; i < pts1i.size(); i++)
 	{
 		Mat x1, x2;
@@ -157,136 +156,139 @@ void reconstruct(vector <Mat> images, vector <Mat> grayImages, String camFile, v
 		seed.push_back(miMatch(ZNCC(pts1i[i], pts2i[i], imag1, imag2), pts1i[i], pts2i[i]));
 		map.push_back(miMatch(ZNCC(pts1i[i], pts2i[i], imag1, imag2), pts1i[i], pts2i[i]));
 	}
-	double eps = 1;
-	make_heap(seed.begin(), seed.end());
-	int T = 2;
-	while (seed.size() != 0) 
+	bool rellenar = true;
+	if(rellenar)
 	{
-		miMatch temp(seed.front().distance, seed.front().p1, seed.front().p2);
-		std::pop_heap(seed.begin(), seed.end());
-		seed.pop_back();
-		double t = 0.01;
-		if(temp.p1.x > 10 && temp.p1.x < imag1.cols - 10 && temp.p1.y > 10 && temp.p1.y < imag1.rows - 10 && 
-		   temp.p2.x > 10 && temp.p2.x < imag1.cols - 10 && temp.p2.y > 10 && temp.p2.y < imag1.rows - 10)
-		{ 
-			for (int i = -2; i < 3; ++i) 
+		double eps = 1;
+		make_heap(seed.begin(), seed.end());
+		int T = 2;
+		while (seed.size() != 0) 
 		{
-			for (int j = -2; j < 3; ++j)
+			miMatch temp(seed.front().distance, seed.front().p1, seed.front().p2);
+			std::pop_heap(seed.begin(), seed.end());
+			seed.pop_back();
+			double t = 0.05;
+			if(temp.p1.x > 10 && temp.p1.x < imag1.cols - 10 && temp.p1.y > 10 && temp.p1.y < imag1.rows - 10 && 
+			   temp.p2.x > 10 && temp.p2.x < imag1.cols - 10 && temp.p2.y > 10 && temp.p2.y < imag1.rows - 10)
+			{ 
+				for (int i = -2; i < 3; ++i) 
 			{
-				if (i != 0 && j != 0) 
+				for (int j = -2; j < 3; ++j)
 				{
-					for (int k = max(-2, i-1); k <= min(2, i+1); ++k)
+					if (i != 0 && j != 0) 
 					{
-						for (int l = max(-2, j - 1); l <= min(2, j + 1); ++l)
+						for (int k = max(-2, i-1); k <= min(2, i+1); ++k)
 						{
-							if (k != 0 && l != 0) 
+							for (int l = max(-2, j - 1); l <= min(2, j + 1); ++l)
 							{
-								double x1 = temp.p1.x + i;
-								double y1 = temp.p1.y + j;
-								double x2 = temp.p2.x + k;
-								double y2 = temp.p2.y + l;
-								Point2f pt1 = Point2f(x1, y1);
-								Point2f pt2 = Point2f(x2, y2);
-								bool encontro = false;
-								for (vector < miMatch >::iterator it = map.begin(); it != map.end(); ++it)
+								if (k != 0 && l != 0) 
 								{
-									if ((abs(pt1.x - it->p1.x) < eps && abs(pt1.y - it->p1.y) < eps )||(abs(pt1.x  -it->p1.x) < eps && abs(pt1.y - it->p1.y) < eps ))
+									float x1 = temp.p1.x + i;
+									float y1 = temp.p1.y + j;
+									float x2 = temp.p2.x + k;
+									float y2 = temp.p2.y + l;
+									Point2f pt1 = Point2f(x1, y1);
+									Point2f pt2 = Point2f(x2, y2);
+									bool encontro = false;
+									for (vector < miMatch >::iterator it = map.begin(); it != map.end(); ++it)
 									{
-										encontro = true;
+										if ((abs(pt1.x - it->p1.x) < eps && abs(pt1.y - it->p1.y) < eps )||(abs(pt1.x  -it->p1.x) < eps && abs(pt1.y - it->p1.y) < eps ))
+										{
+											encontro = true;
+										}
+									}
+									if(!encontro)
+									{
+										double d = ZNCC(pt1, pt2, imag1, imag2);
+										double a = s(pt1, imag1);
+										double b = s(pt2, imag2);
+										if (a > t && b > t && d > 0.8)
+										{
+											local.push_back(miMatch(d, pt1, pt2));
+										}
 									}
 								}
-								if(!encontro)
-								{
-									double d = ZNCC(pt1, pt2, imag1, imag2);
-									double a = s(pt1, imag1);
-									double b = s(pt2, imag2);
-									if (a > t && b > t && d > 0.7)
-									{
-										local.push_back(miMatch(d, pt1, pt2));
-									}
-								}
-								
 							}
 						}
 					}
 				}
 			}
-		}
-		}
-		
-		make_heap(local.begin(), local.end());
-		while (local.size() != 0) 
-		{
-			miMatch temp(local.front().distance, local.front().p1, local.front().p2);
-			std::pop_heap(local.begin(), local.end());
-			local.pop_back();
-			bool encontro = false;
-			for (vector < miMatch >::iterator it = map.begin(); it != map.end(); ++it)
-			{
-				if ((abs(temp.p1.x - it->p1.x) < eps && abs(temp.p1.y - it->p1.y) < eps) || (abs(temp.p1.x - it->p1.x) < eps && abs(temp.p1.y - it->p1.y) < eps))
-				{
-					encontro = true;
-				}
 			}
-			if (!encontro)
-			{
-				Mat x1, x2;
-				vconcat( Mat(temp.p1), Mat::eye(1,1,CV_32FC1), x1);
-				vconcat(Mat(temp.p2), Mat::eye(1, 1, CV_32FC1), x2);
-				Mat m = x2.t()*F*x1;
-				if (abs(m.at<float>(0)) < 0.01)
-				{
-					seed.push_back(temp);
-					push_heap(seed.begin(), seed.end());
-					map.push_back(temp);
-				}
 
-			}
-		}
-		if(map.size()>T)
-		{
-			vector< Vec3b > color;
-			vector < Point2f > densified1, densified2;
-			for (int i = 0; i < map.size(); ++i)
+			make_heap(local.begin(), local.end());
+			while (local.size() != 0) 
 			{
-				densified1.push_back(map[i].p1);
-				densified2.push_back(map[i].p2);
-				color.push_back(images[0].at<Vec3b>(map[i].p1));
-			}
-			T = 2*T;
-			images[0].copyTo(img1);
-			images[1].copyTo(img2);
-			inlier.clear();
-			E = findEssentialMat(densified1, densified2, K, CV_RANSAC, 0.99, 3.0, inlier);
-			F = findFundamentalMat(densified1, densified2, CV_FM_RANSAC, 3.0, 0.99, inlier);
-			F.convertTo(F, CV_32F);
-			for (int i = 0; i < inlier.size(); i++)
-			{
-				if (inlier.at(i) != 0)
+				miMatch temp(local.front().distance, local.front().p1, local.front().p2);
+				std::pop_heap(local.begin(), local.end());
+				local.pop_back();
+				bool encontro = false;
+				for (vector < miMatch >::iterator it = map.begin(); it != map.end(); ++it)
 				{
-					pts2i.push_back(densified2[i]);
-					pts1i.push_back(densified1[i]); //bf.match(query, train)
+					if ((abs(temp.p1.x - it->p1.x) < eps && abs(temp.p1.y - it->p1.y) < eps) || (abs(temp.p1.x - it->p1.x) < eps && abs(temp.p1.y - it->p1.y) < eps))
+					{
+						encontro = true;
+					}
+				}
+				if (!encontro)
+				{
+					Mat x1, x2;
+					vconcat( Mat(temp.p1), Mat::eye(1,1,CV_32FC1), x1);
+					vconcat(Mat(temp.p2), Mat::eye(1, 1, CV_32FC1), x2);
+					Mat m = x2.t()*F*x1;
+					if (abs(m.at<float>(0)) < 0.01)
+					{
+						seed.push_back(temp);
+						push_heap(seed.begin(), seed.end());
+						map.push_back(temp);
+					}
+
 				}
 			}
-			densified1 = pts1i;
-			densified2 = pts2i;
-			for (int i = 0; i < map.size(); ++i)
+			if(map.size()>T)
 			{
-				Scalar color(rand() % 256, rand() % 256, rand() % 256);
-				circle(img1, map[i].p1, 10, color, 6);
-				circle(img2, map[i].p2, 10, color, 6);
+				vector< Vec3b > color;
+				vector < Point2f > densified1, densified2;
+				for (int i = 0; i < map.size(); ++i)
+				{
+					densified1.push_back(map[i].p1);
+					densified2.push_back(map[i].p2);
+					color.push_back(images[0].at<Vec3b>(map[i].p1));
+				}
+				T = 1.5*T;
+				images[0].copyTo(img1);
+				images[1].copyTo(img2);
+				inlier.clear();
+				E = findEssentialMat(densified1, densified2, K, CV_RANSAC, 0.99, 3.0, inlier);
+				F = findFundamentalMat(densified1, densified2, CV_FM_RANSAC, 3.0, 0.99, inlier);
+				F.convertTo(F, CV_32F);
+				for (int i = 0; i < inlier.size(); i++)
+				{
+					if (inlier.at(i) != 0)
+					{
+						pts2i.push_back(densified2[i]);
+						pts1i.push_back(densified1[i]); //bf.match(query, train)
+					}
+				}
+				densified1 = pts1i;
+				densified2 = pts2i;
+				for (int i = 0; i < map.size(); ++i)
+				{
+					Scalar color(rand() % 256, rand() % 256, rand() % 256);
+					circle(img1, map[i].p1, 10, color, 6);
+					circle(img2, map[i].p2, 10, color, 6);
+				}
+				cout << "map size: " << map.size() << endl;
+				/*img1.copyTo(out(rect1));
+				img2.copyTo(out(rect2));
+				namedWindow("rect", CV_WINDOW_KEEPRATIO);
+				imshow("rect", out);
+				waitKey(2000);
+				destroyWindow("rect");*/
 			}
-			cout << "map size: " << map.size() << endl;
-			img1.copyTo(out(rect1));
-			img2.copyTo(out(rect2));
-			namedWindow("rect", CV_WINDOW_KEEPRATIO);
-			imshow("rect", out);
-			waitKey(15000);
-			destroyWindow("rect");
-		}
-		if (map.size() > 150000)
-		{
-			break;
+			if (map.size() > 30000)
+			{
+				break;
+			}
 		}
 	}
 	vector< Vec3b > color;
@@ -302,21 +304,43 @@ void reconstruct(vector <Mat> images, vector <Mat> grayImages, String camFile, v
 	imwrite("res.PNG", out);
 
 	Mat R, tr;
+	float S = 300;
+	//E = findEssentialMat(densified1, densified2, K, CV_RANSAC, 0.99, 4.0, inlier);
+	vector<uchar> inli;
+	for (int i = 0; i < densified1.size(); ++i)
+	{
+		inli.push_back(1);
+	}
+	//recoverPose(E, densified1, densified2, K, R, tr,inli);
 	recoverPose(E, densified1, densified2, K, R, tr);
+	cout << "det(R): " << determinant(R) << endl;
+
+	cout << "Correcting matches using the Optimal Triangulation Method.." << endl;
+	vector<Point2f> corrected1, corrected2;
+	Mat Ftemp(3, 3, CV_32FC1);
+	K.convertTo(K, CV_32FC1);
+	E.convertTo(E, CV_32FC1);
+	Ftemp = K.t()*E*K;
+	cout << "Ftemp: " << Ftemp << endl;
+	correctMatches(F, densified1, densified2, corrected1, corrected2);
+
 	cout << "Triangulating.." << endl;
 	Mat P1, P2;
-	Mat R1 = Mat::eye(3, 3, CV_64FC1);
-	Mat t1 = Mat::zeros(3, 1, CV_64FC1);
+	Mat R1 = Mat::eye(3, 3, CV_32FC1);
+	Mat t1 = Mat::zeros(3, 1, CV_32FC1);
 	hconcat(R1, t1, P1);
-	hconcat(R, tr, P2);
-
+	hconcat(R, S*tr, P2);
+	cout << "P1: " << P1 << endl << "P2: " << P2 << endl;
 	Mat pts4D;
-	triangulatePoints(K*P1, K*P2, densified1, densified2, pts4D);
+
+	P1.convertTo(P1, CV_32F);
+	P2.convertTo(P2, CV_32F);
+	triangulatePoints(K*P1, K*P2, corrected1, corrected2, pts4D);
 	//cout << pts4D << endl;
 	pts4D = pts4D.t();
 	Mat pts3D;
 	convertPointsFromHomogeneous(pts4D, pts3D);
-
+	
 	cout << "Creating PLY file.." << endl;
 	generatePLY("Nube", pts3D, color);
 }
